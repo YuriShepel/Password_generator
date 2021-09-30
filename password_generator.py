17080b45aca6746031443b26da44c4d806e41a0d
YuriShepel <donbidonn@gmail.com>
2021-09-30 11:22:15 +0300

    Finish

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,18 @@
 import random
+
+
 digits = '0123456789'
 lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_.'
 chars = ''
 number_pass = int(input('Введите количество необходимых паролей: '))
-number_char = int(input('Введите количество символов для пароля: '))
-ness_digit = input('Нужно ли включить цыфры в пароль? д - Да, н - Нет')
-ness_apper = input('Включать ли заглавные буквы? д - Да, н - Нет')
-ness_lower = input('Включать ли строчные буквы? д - Да, н - Нет')
-ness_punct = input('Включать ли символы? д - Да, н - Нет')
-exc_ambig = input('Исключать ли неоднозначные символы il1Lo0O? д - Да, н - Нет')
+number_char = input('Введите количество символов для пароля: ')
+ness_digit = input('Нужно ли включить цыфры в пароль? д - Да, н - Нет ')
+ness_apper = input('Включать ли заглавные буквы? д - Да, н - Нет ')
+ness_lower = input('Включать ли строчные буквы? д - Да, н - Нет ')
+ness_punct = input('Включать ли символы? д - Да, н - Нет ')
+exc_ambig = input('Исключать ли неоднозначные символы il1Lo0O? д - Да, н - Нет ')
 if ness_digit == 'д':
     chars += digits
 if ness_apper == 'д':
@@ -22,3 +24,12 @@ if ness_punct == 'д':
 if exc_ambig == 'д':
     for c in 'il1Lo0O':
         chars.replace(c, '')
+
+def generate_password(number_char, chars):
+    password = ''
+    for j in range(int(number_char)):
+        password += random.choice(chars)
+    print(password)
+
+for _ in range(number_pass):
+    generate_password(number_char, chars)
